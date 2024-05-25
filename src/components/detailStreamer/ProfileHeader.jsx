39ddd0a876ea750b68f979687b1e4bf4066dd0f4
LuanardoDX <52100061@student.tdtu.edu.vn>
2024-05-25 18:07:38 +0700

    fix: profile header streamer info page

--- src/components/detailStreamer/ProfileHeader.jsx
+++ src/components/detailStreamer/ProfileHeader.jsx
@@ -11,7 +11,7 @@ const ProfileHeader = ({ streamer }) => {
 
     return (
         <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-            <div className="z-20 h-16 md:h-50">
+            <div className="z-20 h-20 md:h-40 overflow-hidden">
                 <img
                     src={streamer.profile_banner}
                     alt="profile banner"
@@ -19,13 +19,13 @@ const ProfileHeader = ({ streamer }) => {
                 />
             </div>
             <div className="px-3 pb-3 md:px-6 md:pb-6 flex gap-4">
-                <div className="z-30 -mt-6 md:-mt-15 h-20 w-full max-w-20 rounded-full 
-                    bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
+                <div className="z-30 md:mt-4 h-20 w-full max-w-20 rounded-full 
+                    bg-black/20 dark:bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
                     <div className="drop-shadow-2">
                         <img src={streamer.profile_picture} alt="profile" className="rounded-full object-cover" />
                     </div>
                 </div>
-                <div className="space-y-2 mt-3">
+                <div className="space-y-2 md:mt-4">
                     <h3 className="text-sm md:text-3xl font-semibold text-black dark:text-white">
                         {streamer.name}
                     </h3>
