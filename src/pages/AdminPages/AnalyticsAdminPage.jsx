ac9e28fc7b2aad6fb6fb337bfea394bd4ac16d85
Thanh Tan Tran <trantanthanh9a10@gmail.com>
2024-08-02 21:13:11 +0700

    add admin page

---
+++ src/pages/AdminPages/AnalyticsAdminPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnalyticsAdminPage = () => {
+	return (
+		<div>AnalyticsPage</div>
+	)
+}
+
+export default AnalyticsAdminPage
---
+++ src/pages/AdminPages/StreamAdminPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StreamAdminPage = () => {
+	return (
+		<div>StreamAdminPage</div>
+	)
+}
+
+export default StreamAdminPage
