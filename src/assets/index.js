442ec31c5acd9b7b2d05ba9cb25a088c5a3a95a7
Thanh Tan Tran <trantanthanh9a10@gmail.com>
2024-08-10 13:03:40 +0700

    change UI

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,8 @@ import lightmode from './lightmode.jpg';
 import darkmode from './darkmode.jpg';
 import logo from './coding-duo-logo.png';
 import banned from './banned.png';
-import defaultUser from './user.jpg'
+import defaultUser from './user.jpg';
+import statsLatest from './stats-latest.png';
 export {
-	lightmode, darkmode, logo, defaultUser, banned
+	lightmode, darkmode, logo, defaultUser, banned, statsLatest
 }
